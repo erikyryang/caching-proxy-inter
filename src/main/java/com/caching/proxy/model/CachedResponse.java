60326fe269e56f389cd208629e3a9730cef7a12e
erikyryang <erikyryang@gmail.com>
2025-07-14 07:58:50 -0300

    Add serialVersionUID to CachedResponse and create Qodana configuration file

--- src/main/java/com/caching/proxy/model/CachedResponse.java
+++ src/main/java/com/caching/proxy/model/CachedResponse.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpHeaders;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CachedResponse implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private byte[] body;
